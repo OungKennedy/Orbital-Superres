d97e7a04ce37c5a322a3fbdbd636a8965a1e3705
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-27 09:22:01 +0000

    fixed some bugs. enabled landmark detection option. still need to set up push

--- app.py
+++ app.py
@@ -1,45 +1,49 @@
-from flask import Flask, jsonify, request, send_file, g
 import os
 import time
+from io import BytesIO
 
-from Scripts.helpers import feature_loss
-from fastai.basic_train import load_learner
-from fastai.vision import Image
-import torchvision
-import Scripts.utils
-from Scripts.size_change import change_output_size
+from fastai.basic_train import load_learner, np
+from fastai.vision import Image, PIL, pil2tensor, image2np
+from flask import Flask, jsonify, request, send_file, g
+import tempfile 
 
 from werkzeug.utils import secure_filename
 
 from face_detection import LandmarksDetector, image_align
-from utils import feature_loss
+from Scripts.helpers.feature_loss import FeatureLoss
 import __main__
-__main__.FeatureLoss = feature_loss
+__main__.FeatureLoss = FeatureLoss 
 
 app = Flask(__name__)
+#why is this required ?
+app.config['SECRET_KEY'] = 'ysecretkeys'
 ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
-save_dir = tempfile.TemporaryDirectory(dir=static)
+save_dir = tempfile.TemporaryDirectory(dir="static")
 
 #load model from models directory
-model = None 
+model = load_learner('models','resnet34-enhance.pkl')
+print('model loaded')
 
-def load_model():
-    global model
-    model = load_learner('models','resnet34-enhance.pkl')
-    print('model loaded')
-
+@app.route('/_hello_world')
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
+def predict(img):
+    x = pil2tensor(img, np.float32)
+    x.div_(255)
+    output = model.predict(Image(x))[0]
+    return output
+
 def generate_faces(img_path, filename):
+    print("generating faces\n")
     landmarks_detector = LandmarksDetector()
     img_paths = []
     for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(img_path),start=1):
         # Extract face and save at 512px
         img = image_align(img_path, face_landmarks)
         fp_bef = tempfile.NamedTemporaryFile(suffix=str(i)+'bef'+filename, dir=save_dir.name, delete=False)
-        img_save(fp_bef,'PNG')
+        img.save(fp_bef,'PNG')
 
         # Process face and save
         img = img.resize((128,128), PIL.Image.BILINEAR)
@@ -49,10 +53,11 @@ def generate_faces(img_path, filename):
 
         # Add to list
         imgs = (os.path.relpath(fp_bef.name), os.path.relpath(fp_aft.name))
-        img_paths.append(imgs)
+        img_paths.extend(imgs)
     return img_paths
 
 def predict_as_is(img_path, filename):
+    print("predicting as is\n")
     # has issues with scaling
     img = vision.image.open_image(img_path)
     # get scale of image
@@ -62,17 +67,17 @@ def predict_as_is(img_path, filename):
     output_y = 512 * img_scale
 
     # prediction
-    fp_bef = tempfile.NamedTemporaryFile('bef'+filename, dir = save_dir.name, delete=False)
+    fp_bef = tempfile.NamedTemporaryFile(suffix='bef'+filename, dir = save_dir.name, delete=False)
     t1 = time.time()
     output = predict(img)
     print("predict-as-is time: ", time.time() - t1)
     img.save(fp_bef, 'PNG')
-    fp_aft = tempfile.NamedTemporaryFile('aft'+filename, dir=save_dir.name, delete=False)
+    fp_aft = tempfile.NamedTemporaryFile(suffix='aft'+filename, dir=save_dir.name, delete=False)
     output.save(fp_aft)
     
-    img_paths = []
-    img_paths.append(os.path.relpath(fp_bef.name))
-    img_paths.append(os.path.relpath(fp_aft.name))
+    # Add to list
+    imgs = (os.path.relpath(fp_bef.name), os.path.relpath(fp_aft.name))
+    img_paths.extend(imgs)
     return img_paths
 
 '''
@@ -82,10 +87,11 @@ The filepath is sent back to client
 '''
 @app.route("/mobile_predict", methods=['POST'])
 
-def predict():
+def handle_incoming_post():
     try:
         print(request.headers)
-        data = request.files['file']
+        print(request.data)
+        file = request.files['file']
     except:
         keylist = [x for x in request.files]
         keylist_str = ','.join(keylist)
@@ -98,18 +104,38 @@ def predict():
 
         fp = tempfile.NamedTemporaryFile(suffix=filename, dir=save_dir.name, delete=False)
         file.save(fp.name)
-
-        if request.files['do_landmark_detection'] == True:
+        print("file saved as " + str(fp.name) + '\n')
+        # temporary dummy for boolean to do landmark detection
+        do_landmark_detection = True
+        test_dummy = request.form.get("landmark_detection")
+        test_dummy2 = request.values.get("landmark_detection")
+        arg_list = [i for i in request.args]
+        form_list = [i for i in request.form]
+        value_list = [i for i in request.values]
+        print(arg_list)
+        print(form_list)
+        print(value_list)
+        print(test_dummy)
+        print(test_dummy2)
+        if (test_dummy == "true"):
+            print("successfully acquired signal\n")
+        else :
+            print("did not get signal successfully "+'\n')
+        if  do_landmark_detection == True:
+            print("doing landmark detection\n")
             img_paths_list = generate_faces(fp.name, filename)
-            img_paths_str = ','.join(img_paths_list)
             
         else :
+            print("do_landmark_detection != True\n")
             img_paths_list = predict_as_is(fp.name, filename)
-            img_paths_str = ','.join(img_paths_list)
-            
-
+        
+        #prepend url to complete download link
+        img_urls_list = ["http://35.197.17.49/download?filename="+str(i) for i in img_paths_list]
+        img_urls_str = ','.join(img_urls_list)
+        img_urls_str = '%' + img_urls_str + '%'
+        print(img_urls_str + '\n')
     # return filename with tokens to client
-    return jsonify({'filenames':img_paths_str, "status":"ok"})
+    return jsonify({'filenames':img_urls_str, "status":"ok"})
 
 '''
 function that sends file to client
@@ -121,16 +147,12 @@ curl http://6fd9b167.ngrok.io/download?filename="hi-res.png" -o from-server.png
 @app.route("/download", methods=['GET'])
 def return_image():
     # form filepath from arguments provided by user
-    filepath = 'Predicted/' + request.args.get('filename')
-    print(filepath)
+    filepath = request.args.get('filename')
+    print("filepath is " + filepath + '\n')
     try:
         if os.path.isfile(filepath):
             #check download then remove
-            file_handle = open(filepath,'r')
-            yield from file_handle
-            file_handle.close()
-            os.remove(filepath)
-            # return send_file(filepath)
+            return send_file(filepath)
 
         else:
             print("file not found")
@@ -138,9 +160,9 @@ def return_image():
     except Exception as e:
         return jsonify({'message':'error encountered','error message':str(e)})
 
-if __name__ == '__main__':
-    print("Loading PyTorch model and Flask starting server ...")
-    print("Please wait until server has fully started")
-    load_model()
-    print("model_loaded")
-    app.run(host='0.0.0.0', port = 80)
+#if __name__ == '__main__':
+#    print("Loading PyTorch model and Flask starting server ...")
+#    print("Please wait until server has fully started")
+#    load_model()
+#    print("model_loaded")
+#    app.run(host='0.0.0.0', port = 80)
